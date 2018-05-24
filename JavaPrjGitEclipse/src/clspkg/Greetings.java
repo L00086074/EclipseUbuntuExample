ea2db7839851d8e1ae92b9d44ba69dbc4f31cbf2
Colin Kenny L00086074 <L00086074@student.lyit.ie>
2018-05-24 23:15:38 +0100

    On review this will be the second commit

--- JavaPrjGitEclipse/src/clspkg/Greetings.java
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -14,7 +14,7 @@ public class Greetings {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Greeting Colin L00086074");
+		System.out.println("Greeting Colin L00086074,how are things today?");
 
 	}
 
