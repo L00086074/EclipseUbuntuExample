a7f9e32c06ac533cd81c4052874dcb8e88eba5d0
Colin Kenny L00086074 <L00086074@student.lyit.ie>
2018-05-24 22:53:12 +0100

    Have dragged five unstaged items for commit Greetings.java

---
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package clspkg;
+
+/**
+ * @author colin
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greeting Colin L00086074");
+
+	}
+
+}
